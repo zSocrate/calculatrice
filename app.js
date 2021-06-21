b028d3d9cc5d056b33e3155656d0a0ff32e3a933
zSocrate <lu.ilan@free.fr>
2021-06-21 20:50:37 +0200

    End

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Wesh")
+const touches = [...document.querySelectorAll('.bouton')];
+const listKeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.ecran');
+
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+    console.log(valeur, typeof valeur)
+    calculer(valeur)
+})
+
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
+})
+
+const calculer = (valeur) => {
+    if (listKeycode.includes(valeur)) {
+        switch(valeur) {
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listKeycode.indexOf(valeur);
+                const touche = touches[indexKeycode];
+                ecran.textContent += touche.innerHTML;
+        }
+    }
+}
+
+window.addEventListener('error', (e) => {
+    alert('Une erreur est survenue dans ton calcul le sang : ' + e.message)
+})
